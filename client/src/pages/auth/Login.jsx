b0e96ee1480fbdac2c13e74b70d49402abe825f7
Kushal1715 <kushalkhadka77@gmail.com>
2025-01-03 15:02:35 +0545

    login register ui completed

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import CommonForm from "@/components/common/form";
+import { loginFormControls } from "@/config";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const initialFormData = {
+  email: "",
+  password: "",
+};
 
 const AuthLogin = () => {
-  return <div>AuthLogin</div>;
+  const [formData, setFormData] = useState(initialFormData);
+
+  console.log(formData);
+
+  const onSubmit = () => {};
+  return (
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold mb-2 tracking-tight text-foreground">
+          Sign in to your account
+        </h1>
+        <p>
+          Don't have an account?{" "}
+          <Link
+            to="/auth/register"
+            className="font-bold hover:cursor-pointer hover:underline"
+          >
+            Register
+          </Link>
+        </p>
+      </div>
+      <CommonForm
+        formControls={loginFormControls}
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+        buttonText="Login"
+      />
+    </div>
+  );
 };
 
 export default AuthLogin;
--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import CommonForm from "@/components/common/form";
+import { registerFormControls } from "@/config";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const initialFormData = {
+  userName: "",
+  email: "",
+  password: "",
+};
 
 const AuthRegister = () => {
-  return <div>AuthRegister</div>;
+  const [formData, setFormData] = useState(initialFormData);
+
+  console.log(formData);
+
+  const onSubmit = () => {};
+  return (
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold mb-2 tracking-tight text-foreground">
+          Create new account
+        </h1>
+        <p>
+          Already have an account?{" "}
+          <Link
+            to="/auth/login"
+            className="font-bold hover:cursor-pointer hover:underline"
+          >
+            Login
+          </Link>
+        </p>
+      </div>
+      <CommonForm
+        formControls={registerFormControls}
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+        buttonText="Sign Up"
+      />
+    </div>
+  );
 };
 
 export default AuthRegister;
