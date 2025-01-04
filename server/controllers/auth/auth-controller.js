8579e40ff7afd5f289992282cee9ab4f23d4a342
Kushal1715 <kushalkhadka77@gmail.com>
2025-01-04 17:25:42 +0545

    making auth controller to login

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -1,10 +1,18 @@
 const bcrypt = require("bcryptjs");
 const User = require("../../models/User");
+const jwt = require("jsonwebtoken");
 
 //register
 const registerUser = async (req, res) => {
   const { userName, email, password } = req.body;
   try {
+    const checkUser = await User.findOne({ email });
+    if (checkUser) {
+      return res.json({
+        success: false,
+        message: "user already exists with this email",
+      });
+    }
     const hashedPassword = await bcrypt.hash(password, 12);
     const newUser = new User({
       userName,
@@ -31,6 +39,40 @@ const registerUser = async (req, res) => {
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
   try {
+    const checkUser = await User.findOne({ email });
+    if (!checkUser) {
+      return res.json({
+        success: false,
+        message: "user does not exist with this email",
+      });
+    }
+
+    const checkPasswordMatch = await bcrypt.compare(
+      password,
+      checkUser.password
+    );
+    if (!checkPasswordMatch) {
+      return res.json({
+        success: false,
+        message: "incorrect password, please try again",
+      });
+    }
+
+    const token = jwt.sign(
+      { id: checkUser._id, role: checkUser.role, email: checkUser.email },
+      "SECRET_KEY",
+      { expiresIn: "60m" }
+    );
+
+    res.cookie("token", token, { httpOnly: true, secure: false }).json({
+      success: true,
+      message: "logged in successfully",
+      user: {
+        email: checkUser.email,
+        role: checkUser.role,
+        id: checkUser._id,
+      },
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({
@@ -44,4 +86,4 @@ const loginUser = async (req, res) => {
 
 //middleware
 
-module.exports = { registerUser };
+module.exports = { registerUser, loginUser };
