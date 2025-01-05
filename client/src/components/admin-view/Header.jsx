4997861783202746a1e7025db02d5b2b79e6a082
Kushal1715 <kushalkhadka77@gmail.com>
2025-01-05 16:58:23 +0545

    admin header and sidebar completed

--- client/src/components/admin-view/Header.jsx
+++ client/src/components/admin-view/Header.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Button } from "../ui/button";
+import { AlignJustify, LogOut, Menu } from "lucide-react";
 
 const AdminHeader = () => {
-  return <div>AdminHeader</div>;
+  return (
+    <div className="flex items-center justify-between px-4 py-3 bg-background border-b">
+      <Button className="lg:hidden sm:block">
+        <AlignJustify />
+        <span className="sr-only">Toggle menu</span>
+      </Button>
+      <div className="flex flex-1 justify-end">
+        <Button className="flex gap-2 items-center text-sm font-medium shadow rounded-md">
+          <LogOut />
+          Logout
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default AdminHeader;
--- client/src/components/admin-view/Sidebar.jsx
+++ client/src/components/admin-view/Sidebar.jsx
@@ -1,7 +1,67 @@
+import {
+  BadgeCheck,
+  ChartNoAxesCombined,
+  LayoutDashboard,
+  Menu,
+  ShoppingBasket,
+} from "lucide-react";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const adminSidebarMenuItems = [
+  {
+    id: "dashboard",
+    label: "Dashboard",
+    path: "/admin/dashboard",
+    icon: <LayoutDashboard />,
+  },
+  {
+    id: "products",
+    label: "Products",
+    path: "/admin/products",
+    icon: <ShoppingBasket />,
+  },
+  {
+    id: "orders",
+    label: "Orders",
+    path: "/admin/orders",
+    icon: <BadgeCheck />,
+  },
+];
+
+const MenuItems = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="mt-10 flex flex-col gap-6">
+      {adminSidebarMenuItems.map((item) => (
+        <div
+          className="flex items-center gap-3 cursor-pointer text-muted-foreground hover:bg-muted hover:text-foreground py-2 px-3 rounded-md text-lg"
+          onClick={() => navigate(`${item.path}`)}
+        >
+          {item.icon}
+          {item.label}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 const AdminSidebar = () => {
-  return <div>AdminSidebar</div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="w-64 hidden lg:flex flex-col bg-background border-r p-6">
+        <div
+          className="flex items-center gap-2 font-extrabold text-2xl cursor-pointer"
+          onClick={() => navigate("/admin/dashboard")}
+        >
+          <ChartNoAxesCombined />
+          Admin Panel
+        </div>
+        <MenuItems />
+      </div>
+    </>
+  );
 };
 
 export default AdminSidebar;
