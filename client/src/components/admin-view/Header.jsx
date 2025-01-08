c23c44bd4c4cf47ebd60596c09886b98ac050432
Kushal1715 <kushalkhadka77@gmail.com>
2025-01-08 13:29:53 +0545

    handling logout

--- client/src/components/admin-view/Header.jsx
+++ client/src/components/admin-view/Header.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { Button } from "../ui/button";
 import { AlignJustify, LogOut, Menu } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "@/store/authSlice";
+import { useNavigate } from "react-router-dom";
+import { useToast } from "@/hooks/use-toast";
 
 const AdminHeader = ({ setOpenSidebar }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { toast } = useToast();
+
   return (
     <div className="flex items-center justify-between px-4 py-3 bg-background border-b">
       <Button
@@ -13,7 +21,16 @@ const AdminHeader = ({ setOpenSidebar }) => {
         <span className="sr-only">Toggle menu</span>
       </Button>
       <div className="flex flex-1 justify-end">
-        <Button className="flex gap-2 items-center text-sm font-medium shadow rounded-md">
+        <Button
+          className="flex gap-2 items-center text-sm font-medium shadow rounded-md"
+          onClick={() => {
+            navigate("/auth/login");
+            toast({
+              title: "logged out successfully",
+            });
+            dispatch(logout());
+          }}
+        >
           <LogOut />
           Logout
         </Button>
--- client/src/store/authSlice.js
+++ client/src/store/authSlice.js
@@ -34,6 +34,18 @@ export const loginUser = createAsyncThunk("/auth/login", async (formData) => {
   return response.data;
 });
 
+export const logout = createAsyncThunk("/auth/logout", async () => {
+  const response = await axios.post(
+    "http://localhost:5000/api/auth/logout",
+    {},
+    {
+      withCredentials: true,
+    }
+  );
+
+  return response.data;
+});
+
 export const checkAuth = createAsyncThunk("/auth/checkauth", async () => {
   const response = await axios.get(
     "http://localhost:5000/api/auth/check-auth",
@@ -91,6 +103,19 @@ const authSlice = createSlice({
         state.isAuthenticated = action.payload.success;
       })
       .addCase(checkAuth.rejected, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(logout.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(logout.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(logout.rejected, (state, action) => {
         state.isLoading = false;
         state.user = null;
         state.isAuthenticated = false;
