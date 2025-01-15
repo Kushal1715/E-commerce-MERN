1b7aa4f37dc699e549d76e380cbcd085a83696c6
Kushal1715 <kushalkhadka77@gmail.com>
2025-01-15 23:15:01 +0545

    addling nav items in header

--- client/src/components/shopping-view/Header.jsx
+++ client/src/components/shopping-view/Header.jsx
@@ -1,7 +1,54 @@
+import { HousePlug, Menu } from "lucide-react";
 import React from "react";
+import { Link } from "react-router-dom";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { Button } from "../ui/button";
+import { useSelector } from "react-redux";
+import { shoppingViewHeaderMenuItems } from "@/config";
+
+const MenuItems = () => {
+  return (
+    <nav className="flex flex-col lg:flex-row lg:items-center lg:mb-0 mb-3 gap-6">
+      {shoppingViewHeaderMenuItems.map((menuItem) => (
+        <Link
+          key={menuItem.id}
+          to={menuItem.path}
+          className="text-sm font-medium"
+        >
+          {menuItem.label}
+        </Link>
+      ))}
+    </nav>
+  );
+};
 
 const ShoppingHeader = () => {
-  return <div>ShoppingHeader</div>;
+  const { isAuthenticated } = useSelector((state) => state.auth);
+  return (
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="flex h-16 items-center justify-between px-4 md:px-6">
+        <Link to="/shop/home" className="flex items-center gap-2">
+          <HousePlug className="h-6 w-6" />
+          <span className="font-bold">ECommerce</span>
+        </Link>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon" className="lg:hidden">
+              <Menu className="h-6 w-6" />
+              <span className="sr-only">Toggle header menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" className="w-full max-w-xs">
+            <MenuItems />
+          </SheetContent>
+        </Sheet>
+        <div className="hidden lg:block">
+          <MenuItems />
+        </div>
+        {isAuthenticated && <div></div>}
+      </div>
+    </header>
+  );
 };
 
 export default ShoppingHeader;
