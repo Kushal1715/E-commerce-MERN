84bf13623c35462d4f60f8b6d091ab438dda79b6
Kushal1715 <kushalkhadka77@gmail.com>
2025-01-16 14:04:30 +0545

    headers in product listing page

--- client/src/pages/shopping-view/Listing.jsx
+++ client/src/pages/shopping-view/Listing.jsx
@@ -1,10 +1,50 @@
 import ProductFilter from "@/components/shopping-view/filter";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+} from "@/components/ui/dropdown-menu";
+import { Separator } from "@/components/ui/separator";
+import { sortOptions } from "@/config";
+import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { ArrowUpDownIcon } from "lucide-react";
 import React from "react";
 
 const ShoppingListing = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6">
       <ProductFilter />
+      <div className="w-full bg-background rounded-lg shadow">
+        <div className="flex items-center justify-between border-b p-4">
+          <h1 className="font-extrabold text-lg">All Products</h1>
+          <div className="flex items-center gap-3">
+            <h3 className="text-muted-foreground">10 Products</h3>
+
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="flex items-center gap-1"
+                >
+                  <ArrowUpDownIcon className="h-4 w-4" /> <span>Sort by</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end" className="w-[200px]">
+                <DropdownMenuRadioGroup>
+                  {sortOptions.map((option) => (
+                    <DropdownMenuRadioItem key={option.id}>
+                      {option.label}
+                    </DropdownMenuRadioItem>
+                  ))}
+                </DropdownMenuRadioGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
