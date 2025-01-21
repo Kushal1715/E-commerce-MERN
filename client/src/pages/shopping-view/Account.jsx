9a6418b14ed5d7be90cad1d75a04c55c65582cc0
Kushal1715 <kushalkhadka77@gmail.com>
2025-01-21 14:24:07 +0545

    adding tab component from shadcn ui

--- client/src/pages/shopping-view/Account.jsx
+++ client/src/pages/shopping-view/Account.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import accImg from "../../assets/account.jpg";
 
 const ShoppingAccount = () => {
-  return <div>ShoppingAccount</div>;
+  return (
+    <div className="flex flex-col">
+      <div className="relative h-[300px] w-full overflow-hidden">
+        <img
+          src={accImg}
+          className="h-full w-full object-cover object-center"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ShoppingAccount;
